f4b176cefaac5c02c8a61a34436ba294c14566bb
Sergey Mironov <ka6ah505@gmail.com>
2014-09-01 13:01:43 +0400

    changed animation for 5 page

--- pages/page5.lua
+++ pages/page5.lua
@@ -18,7 +18,7 @@ local scaleEzik
 local function createAnimation()
     -- body
     --sheetImage = graphics.newImageSheet( "animation/animation_5.png", { x=0, y=0, width=512, height=558, numFrames=3 } )
-    instance = display.newSprite( sheet4, { name="ezik", start=1, count=3, time=1000 } )
+    instance = display.newSprite( sheet4, { name="ezik", start=1, count=4, time=1000 } )
     instance.anchorX, instance.anchorY = 0, 0
     instance.x = crW+20
     instance.y = crH-150
